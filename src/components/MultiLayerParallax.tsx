6d2eae2ae50bbf6ebce794589ddf14cbee5de92a
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-08-23 14:40:10 -0300

    creating MultiLayerParallax component

---
+++ src/components/MultiLayerParallax.tsx
@@ -0,0 +1,8 @@
+import { motion, useScroll, useTransform } from 'framer-motion'
+import React, { useRef } from 'react'
+
+export default function MultiLayerParallax() {
+    return (
+      <div></div>
+    )
+}
